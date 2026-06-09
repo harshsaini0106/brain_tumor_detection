8b8731d664dd8d902e64d118fa417182f2331b19
Harsh Saini <sainiharsh6544@gmail.com>
2026-06-09 07:35:54 +0530

    upload complete project

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+
+model = load_model("brain_tumor_cnn.keras")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    file = request.files['image']
+
+    file_bytes = np.frombuffer(
+        file.read(),
+        np.uint8
+    )
+
+    img = cv2.imdecode(
+        file_bytes,
+        cv2.IMREAD_GRAYSCALE
+    )
+
+    img = cv2.resize(img, (128,128))
+    img = img.astype("float32") / 255.0
+
+    img = np.expand_dims(img, axis=-1)
+    img = np.expand_dims(img, axis=0)
+
+    prob = model.predict(img)[0][0]
+
+    prediction = "Tumor" if prob > 0.5 else "No Tumor"
+
+    return jsonify({
+        "prediction": prediction,
+        "probability": float(prob)
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
